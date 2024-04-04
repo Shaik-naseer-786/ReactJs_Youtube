eb8c7423702e447a97e8c562fc1926a009e53543
Naseer <sknaseer9347@gmail.com>
2024-04-04 11:41:25 +0530

    class 4

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function Myapp(){
+    return(
+        <>
+        <h1>Hello naseer beta</h1>
+        </>
+    )
+}
+const ReactElement={
+    type :'a',
+    props:{
+        href:'https://google.com',
+        target:'_blanck',
 
+    },
+    children:'click here open google'
+}
+const AnotherElement=(
+    <a href='https://google.com' target='_blank'>Visit to google</a>
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    AnotherElement
   
 )
